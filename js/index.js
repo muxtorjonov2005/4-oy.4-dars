ec731ec563c7494d0159b040b4901213c3fd74f0
muxtorjonov2005 <muxtorjonoveldiyor@gmail.com>
2024-06-15 14:39:46 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,275 @@
+// 1-masala
+// let a = +prompt("ixtiori son")
+// let b = +prompt("ixtiori son")
+// let c = +prompt("ixtiori son")
+// if ((a > b || b > c) && c > 0){
+//     console.log("shart bajarildi")
+// } else{
+//     console.log("shart bajarilmadi");
+// }
+
+// 2-masala
+// let str1 = prompt('ixtiyori')
+// let str2 = prompt('ixtiyori')
+// if ((str1.length > 5 && str1.includes('a')) || str2.startsWith('b')){
+//     console.log('shart bajarildi')
+// } else{
+//     console.log('shart bajarilmadi')
+// }
+
+// 3-masala
+// let a = +prompt('son')
+// let b = +prompt('son')
+// let c = +prompt('son')
+// let d = +prompt('son')
+// if ((a + b + c + d > 50) && a > 10 || b > 10 || c > 10 || d > 10 ){
+//     console.log('yigindi katta yoki hech bolmaganda biri katta');
+// } else{
+//     console.log('yigindi kichkina');
+// }
+
+// 4-masala
+// let a = +prompt('son')
+// let b = +prompt('son')
+// if (a * b < 100 || a+ b > 20 && a % 2 == 0) {
+//     console.log('shart bajarildi');
+// } else {
+//     console.log('bajarilmadi');
+// }
+
+// 5-masala
+// let s = +prompt('yozuv')
+// let n = +prompt('son')
+// if (s.length > n / 2 || n > 50)  {
+//     console.log('shart bajarildi')
+// } else{
+//     console.log('bajarilmadi')
+// }
+
+// 6-masala
+// let a = +prompt('son')
+// let b = +prompt('yozuv')
+// if (a % b == 0 && b > 0 || b % 5 == 0){
+//     console.log('shart bajarildi');
+// } else{
+//     console.log('shart bajarilmadi')
+// }
+
+// 7-masala
+// let a = +prompt('sonni kirriting')
+// let b = +prompt('sonni kirriting')
+// if (a > 0 && b > 0 && c > 0 && b < a * a || c % 2 == 1){
+//     console.log('shart bajarildi');
+// } else{
+//     console.log('bajarilmadi');
+// }
+
+// 8-masala
+// let str1 = +prompt('yozuv')
+// let str2 = +prompt('yozuv')
+// let num = +prompt('sonni kirriting')
+// if (str1.length > num && str2.startsWith('hello') || num % 2 == 0){
+//     console.log('shart bajarildi');
+// } else{
+//     console.log('bajarilmadi');
+// }
+
+// 9-masala
+// let a = +prompt('sonni kirriting')
+// let b = +prompt('sonni kirriting')
+// let c = +prompt('sonni kirriting')
+// if ((a + b + c) / 3 > 10 && a > b || c < 15){
+//     console.log('shart bajarildi');
+// } else{
+//     console.log('bajarilmadi');
+// }
+
+// 10-masala
+// let str1 = +prompt('yozuv kirriting')
+// let str2 = +prompt('yozuv kirriting')
+// if (str1.length > 8 && str1.includes('e') || str2.length < 5 && str2.endsWidth('y')){
+//     console.log('shart bajarildi');
+// } else{
+//     console.log('bajarilmadi');
+// }
+
+// 11-masala
+// a > 0 ? console.log('musbat') : console.log('manfiy');
+
+// 12-masala
+// a > b ? console.log('birinchi son katta') : console.log('ikkinchi son katta');
+
+// 13-masala
+// str.length > 5 ? console.log('uzun string') : console.log('qisqa string')
+
+// 14-masala
+// num % 2 == 0 ? console.log('juft') : console.log('toq');
+
+// 15-masala
+// num > 0 ? console.log('musbat son') : console.log('nol yoki manfiy son');
+
+// 16-masala
+// let num = +prompt("1dan 7gacha sonni kirriting");
+// switch (num) {
+//   case 1:
+//     console.log("duyshanba");
+//     break;
+//   case 2:
+//     console.log("seyshanba");
+//     break;
+//   case 3:
+//     console.log("chorshanba");
+//     break;
+//   case 4:
+//     console.log("payshanba");
+//     break;
+//   case 5:
+//     console.log("juma");
+//     break;
+//   case 6:
+//     console.log("shanba");
+//     break;
+//   case 7:
+//     console.log("yakshanba");
+//     break;
+//   default:
+//     console.log("bunday kun yoq");
+//     break;
+// }
+
+// 17-masala
+// let num = +prompt(" 1dan 12gacha sonni kirriting");
+// switch (num) {
+//   case 1:
+//     console.log("yanvar");
+//     break;
+//   case 2:
+//     console.log("fevral");
+//     break;
+//   case 3:
+//     console.log("mart");
+//     break;
+//   case 4:
+//     console.log("aprel");
+//     break;
+//   case 5:
+//     console.log("may");
+//     break;
+//   case 6:
+//     console.log("iyun");
+//     break;
+//   case 7:
+//     console.log("iyul");
+//     break;
+//   case 8:
+//     console.log("avgust");
+//     break;
+//   case 9:
+//     console.log("sentabr");
+//     break;
+//   case 10:
+//     console.log("oktabr");
+//     break;
+//   case 11:
+//     console.log("noyabr");
+//     break;
+//   case 12:
+//     console.log("dekabr");
+//     break;
+//   default:
+//     console.log("bunday oy yoq");
+//     break;
+// }
+
+// 18-masala
+// let num = +prompt(" 1dan 12gacha sonni kirriting");
+// switch (num) {
+//   case 1:
+//   case 2:
+//   case 12:
+//     console.log("qish");
+//     break;
+//   case 3:
+//   case 4:
+//   case 5:
+//     console.log("bahor");
+//     break;
+//   case 6:
+//   case 7:
+//   case 8:
+//     console.log("yoz");
+//     break;
+//   case 9:
+//   case 10:
+//   case 11:
+//     console.log("kuz");
+//     break;
+//   default:
+//     console.log("bunday fasl yoq");
+//     break;
+// }
+
+// 19-masala
+// let str = +prompt("A dan F gacha harf kirriting");
+// switch (str) {
+//   case A:
+//     console.log("alo");
+//     break;
+//   case B:
+//     console.log("yaxshi");
+//     break;
+//   case C:
+//     console.log("ortacha");
+//     break;
+//   case D:
+//     console.log("qoniqarli");
+//     break;
+//   case F:
+//     console.log("qoniqarsiz");
+//     break;
+//   default:
+//     console.log("buyday ball yoq");
+//     break;
+// }
+
+// 20-masala
+// let num = +prompt(" 1dan 3gacha sonni kirriting");
+// switch (num) {
+//   case 1:
+//     console.log("birinchi");
+//     break;
+//   case 2:
+//     console.log("ikkinchi");
+//     break;
+//   case 3:
+//     console.log(uchinchi);
+//     break;
+//   default:
+//     console.log("nomalum qiyymat");
+//     break;
+// }
+
+// 21-masala
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i * i);
+// }
+
+// 22-masala
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i)
+// }
+
+// 23-masala
+// for ()
+
+// 24-masala
+// for (let i = 2; i <= 20; i += 2) {
+//     console.log(i)
+// }
+
+// 25-masala
+
+// 26-masala
+
+// 27-masala
+
